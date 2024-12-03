2244901e518bd2877043c2e0b27c542515595ebf
Minsoo Cho <textign@gmail.com>
2024-12-03 19:04:13 +0900

    fix: Button 컴포넌트 오류 수정

--- src/app/(providers)/(root)/(main)/page.tsx
+++ src/app/(providers)/(root)/(main)/page.tsx
@@ -11,20 +11,20 @@ const MainPage = () => {
         <Logo size="lg" />
         <AppDescription />
         <div className="w-full flex flex-col justify-center gap-[10px]">
-          <Button content="로그인" type="lg" textColor="text-white" bgColor="bg-main-4" />
-          <Button content="회원가입" type="lg" textColor="text-white" bgColor="bg-blue-4" />
-          <Button content="카카오 로그인하기" type="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
-          <Button content="구글 로그인하기" type="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
+          <Button content="로그인" types="lg" textColor="text-white" bgColor="bg-main-4" />
+          <Button content="회원가입" types="lg" textColor="text-white" bgColor="bg-blue-4" />
+          <Button content="카카오 로그인하기" types="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
+          <Button content="구글 로그인하기" types="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
         </div>
         <div className="mt-5 w-full flex flex-col gap-3">
           버튼 예시
-          <Button content="type: lg" type="lg" textColor="text-white" bgColor="bg-main-4" />
-          <Button content="type: lg 아웃라인" type="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
-          <Button content="type: md" type="md" textColor="text-white" bgColor="bg-main-4" />
-          <Button content="type: md 아웃라인" type="md" textColor="text-gray-2" outlineColor="border-gray-2" />
-          <Button content="type: sm" type="sm" textColor="text-white" bgColor="bg-main-4" />
-          <Button content="type: sm 아웃라인" type="sm" textColor="text-gray-2" outlineColor="border-gray-2" />
-          <Button content="type: add" type="add" />
+          <Button content="type: lg" types="lg" textColor="text-white" bgColor="bg-main-4" />
+          <Button content="type: lg 아웃라인" types="lg" textColor="text-gray-2" outlineColor="border-gray-2" />
+          <Button content="type: md" types="md" textColor="text-white" bgColor="bg-main-4" />
+          <Button content="type: md 아웃라인" types="md" textColor="text-gray-2" outlineColor="border-gray-2" />
+          <Button content="type: sm" types="sm" textColor="text-white" bgColor="bg-main-4" />
+          <Button content="type: sm 아웃라인" types="sm" textColor="text-gray-2" outlineColor="border-gray-2" />
+          <Button content="type: add" types="add" />
         </div>
         <div className="mt-5 w-full flex flex-col gap-3">
           인풋 예시

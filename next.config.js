a1e6cbf2e5399ecb68f2552a3c9888d5a856ed6c
hyorimhan <mynameishan8@gmail.com>
2024-12-24 13:20:19 +0900

    bugfix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -15,21 +14,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/webp"],
   },
-  webpack(config, { dev }) {
-    if (!dev) {
-      config.optimization.minimize = true;
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-          },
-        })
-      );
-    }
-    return config;
-  },
+
   async headers() {
     return [
       {
